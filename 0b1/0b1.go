157b974fa5bf0a8b30802f43d267bee8b5efd7f2
Polariusz <truepolariusz@gmail.com>
2025-05-02 19:49:58 +0200

    feat: added a form for the mqtt options and a respose text for testing purposes

--- 0b1/0b1.go
+++ 0b1/0b1.go
@@ -1,16 +1,62 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
-
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+type MqttUserOpt struct {
+	ClientId string
+	Ip string
+	Topic string
 }
 
+func printMqttUserCon(mqttUserOpt *MqttUserOpt) {
+	fmt.Println("clientId : ", mqttUserOpt.ClientId)
+	fmt.Println("ip       : ", mqttUserOpt.Ip)
+	fmt.Println("topic    : ", mqttUserOpt.Topic)
+}
 
+func connectToMqtt(mqttUserOpt MqttUserOpt) int {
+	printMqttUserCon(&mqttUserOpt)
+	if mqttUserOpt.ClientId == "1" {
+		return 69
+	}
+	return 0
+}
+
+func main() {
+	app := app.New()
+	window := app.NewWindow("MQTT-Explorer.lab")
+
+	clientIdEntry := widget.NewEntry()
+	ipEntry := widget.NewEntry()
+	topicEntry := widget.NewEntry()
+	mqttUserOptResponsiveLabel := widget.NewLabel("")
+
+	mqttUserOptForm := &widget.Form {
+		Items: []*widget.FormItem {
+			{Text: "Your ID", Widget: clientIdEntry},
+			{Text: "IP to connect to", Widget: ipEntry},
+			{Text: "Topic to sub to", Widget: topicEntry},
+		},
+		OnSubmit: func() {
+			res := connectToMqtt(MqttUserOpt{clientIdEntry.Text, ipEntry.Text, topicEntry.Text})
+			if res == 69 {
+				mqttUserOptResponsiveLabel.SetText("You fool!!")
+			} else {
+				mqttUserOptResponsiveLabel.SetText("")
+			}
+		},
+	}
+
+	grid := container.New(layout.NewGridLayout(2), mqttUserOptForm, mqttUserOptResponsiveLabel)
+
+	window.SetContent(grid)
+
+	window.ShowAndRun()
+}
