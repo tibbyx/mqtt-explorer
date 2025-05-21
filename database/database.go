79e2669c5001d2609d9ffa2aba44e709ea35c8dd
Polariusz <truepolariusz@gmail.com>
2025-05-21 22:18:15 +0200

    feat: implemented Broker insertion and query functionality

--- database/database.go
+++ database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"time"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -93,4 +94,129 @@ func SetupDatabase(con *sql.DB) error {
 	}
 
 	return nil
+}
+
+// | Date of change | By        | Comment |
+// +----------------+-----------+---------+
+// | 2025-05-21     | Polariusz | Created |
+//
+// # Struct to Table Mapping
+//
+// | Struct InsertBroker    | Table Broker          |
+// +------------------------+-----------------------+
+// |                        | ID Integer            |
+// | Ip string              | Ip Text               |
+// | Port int               | Port Integer          |
+// |                        | CreationDate DateTime |
+//
+// # Used in
+// - SelectBrokerList()
+//
+// # Author
+// - Polariusz
+type InsertBroker struct {
+	Ip string
+	Port int
+}
+
+// | Date of change | By        | Comment |
+// +----------------+-----------+---------+
+// | 2025-05-21     | Polariusz | Created |
+//
+// # Arguments
+// - con *sql.DB        : It's a connection to the database that is used here to insert stuff in.
+// - broker InsertBroker: The struct that will be written into table `Broker`, if it doesn't exists.
+//
+// # Description
+// - The function shall insert the argument `broker` with the current date into table Broker from connected to database argument `con`.
+// - The insertion shall only happen if the `broker` is not in the database.
+//
+// # Tables Affected
+// - Broker
+//   - INSERT
+//   - SELECT (Subquery)
+//
+// # Returns
+// - error if something bad happened. It can happen if the database that is connected does not have table Broker. In this case, please use functions `OpenDatabase()` and `SetupDatabase()` to set-up the database.
+//
+// # Author
+// - Polariusz
+func InsertNewBroker(con *sql.DB, broker InsertBroker) error {
+	// I insert the arg broker while checking if it isn't in the database. If it is, the insertion will not happen.
+	stmt, err := con.Prepare("INSERT INTO Broker(Ip, Port, CreationDate) SELECT ?, ?, ? WHERE NOT EXISTS(SELECT 1 FROM Broker WHERE Ip = ? AND Port = ?)");
+	if err != nil {
+		return fmt.Errorf("Skill issues\nErr: %s\n", err)
+	}
+
+	if _, err := stmt.Exec(broker.Ip, broker.Port, time.Now(), broker.Ip, broker.Port); err != nil {
+		return fmt.Errorf("Skill issues\nErr: %s\n", err)
+	}
+
+	return nil
+}
+
+// | Date of change | By        | Comment |
+// +----------------+-----------+---------+
+// | 2025-05-21     | Polariusz | Created |
+//
+// # Struct to Table Mapping
+//
+// | Struct SelectBroker    | Table Broker          |
+// +------------------------+-----------------------+
+// | Id int                 | ID Integer            |
+// | Ip string              | Ip Text               |
+// | Port int               | Port Integer          |
+// | CreationDate time.Time | CreationDate DateTime |
+//
+// # Used in
+// - SelectBrokerList()
+//
+// # Author
+// - Polariusz
+type SelectBroker struct {
+	Id int
+	Ip string
+	Port int
+	CreationDate time.Time
+}
+
+// | Date of change | By        | Comment |
+// +----------------+-----------+---------+
+// | 2025-05-21     | Polariusz | Created |
+//
+// # Arguments
+// - con *sql.DB: It's a connection to the database.
+//
+// # Description
+// - Queries the table Broker from declared in function `SetupDatabase()` Tables.
+// - It selects all rows.
+//
+// # Tables Affected
+// - Broker
+//   - SELECT
+//
+// # Returns
+// - A list of struct `SelectBroker`
+// - error can happen if the database that is connected does not have table Broker. In this case, please use functions `OpenDatabase()` and `SetupDatabase()` to set-up the database.
+//
+// # Author
+// - Polariusz
+func SelectBrokerList(con *sql.DB) ([]SelectBroker, error) {
+	var brokerList []SelectBroker
+	rows, err := con.Query("Select * from Broker")
+	if err != nil {
+		return nil, fmt.Errorf("Skill issues\nErr: %s\n", err)
+	}
+
+	for rows.Next() {
+		var Id int
+		var Ip string
+		var Port int
+		var CreationDate time.Time
+
+		rows.Scan(&Id, &Ip, &Port, &CreationDate)
+		brokerList = append(brokerList, SelectBroker{Id, Ip, Port, CreationDate})
+	}
+
+	return brokerList, nil
 }
