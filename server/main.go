3d6ef9cfbdb187d52e7e56d66c92bdbad963ecee
Polariusz <truepolariusz@gmail.com>
2025-06-08 19:49:19 +0200

    fix: forgor to add the stuff to topicResult

--- server/main.go
+++ server/main.go
@@ -529,6 +529,7 @@ func PostTopicSubscribeHandler(serverState *ServerState) fiber.Handler {
 				// SUBSCRIBE
 				if token := serverState.mqttClient.Subscribe(toSubTopic, 0, nil); token.Wait() && token.Error() != nil {
 					fmt.Printf("ERROR: Subscription to topic %s failed!\n", toSubTopic)
+					topicResult[toSubTopic] = TopicResult{"BigError", err.Error()}
 					continue
 				}
 				// INSERT TO TOPIC
@@ -551,6 +552,7 @@ func PostTopicSubscribeHandler(serverState *ServerState) fiber.Handler {
 				// SUBSCRIBE
 				if token := serverState.mqttClient.Subscribe(toSubTopic, 0, nil); token.Wait() && token.Error() != nil {
 					fmt.Printf("ERROR: Subscribtion to topic %s failed!\n", toSubTopic)
+					topicResult[toSubTopic] = TopicResult{"BigError", err.Error()}
 					continue
 				}
 				// INSERT TO USERTOPICSUBSCRIBED
